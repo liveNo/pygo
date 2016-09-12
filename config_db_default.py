bdb7adca5563dd2887957ec966bbe7dcbdc40afb
xululu <xuyanlu0424@163.com>
2016-09-13 00:32:14 +0800

    Init Config Database

---
+++ config_db_default.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding=utf-8 -*-
+
+
+configs = {
+        'ssh1': {
+            'host': '192.168.1.122',
+            'port': 9922,
+            'password': 'asdf23@#$@asdf',
+            'key':''
+            }
+        }
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding=utf-8 -*-
+import config_db_default
+configs =  config_db_default.configs
+
+
+print configs['ssh1']['host']
+
+
