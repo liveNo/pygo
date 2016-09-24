90bedc4f34bebfd6de15b7db5c93a8ca02c06499
xululu <xuyanlu0424@163.com>
2016-09-24 18:58:39 +0800

    format output

--- pygoStaff.py
+++ pygoStaff.py
@@ -13,7 +13,15 @@ class Pygo (object):
         tip_line    = '##       Python SSH Login\r\n'
         end_line    = '###################################\r\n\r\n'
 
-        bannar_line =  '选择器|       主机    |  端口 | 登录用户 |  说明\r\n'
+        # bannar_line =  '选择器|       主机    |  端口 | 登录用户 |  说明\r\n'
+
+        first_option = '选择器'
+        second_option = '主机'
+        third_option = '端口'
+        fourth_option = '登录用户'
+        fifth_option = '说明'
+
+        bannar_line = "%s|%s|%s|%s|%s" % (first_option.center(20), second_option.center(20), third_option.center(20), fourth_option.center(20), fifth_option.center(20)) 
 
         return start_line+tip_line+end_line+bannar_line
 
